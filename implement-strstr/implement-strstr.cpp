167e48c7e81eb2519a45e197655e0026fed57dfa
abahnasy <asbahnasy@gmail.com>
2021-06-28 02:19:03 +0200

    Time: 4 ms (77.13%), Space: 6.9 MB (40.51%) - LeetHub

---
+++ implement-strstr/implement-strstr.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int idx = haystack.find(needle);
+        if(idx != string::npos) {
+            return idx;
+        } else {
+            return -1;
+        }
+    }
+};
