00d01f08c7561770b7d69d3f16d89f518510369d
abahnasy <asbahnasy@gmail.com>
2021-06-29 07:11:18 +0200

    Time: 4 ms (65.39%), Space: 6.5 MB (75.66%) - LeetHub

--- length-of-last-word/length-of-last-word.cpp
+++ length-of-last-word/length-of-last-word.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int last = 0;
         int c = 0;
-        for(int i = 0; i < s.length(); ++i) {
-            if(s[i] == ' ' && c != 0) {
-                last = c;
-                c = 0;
-            }else if (s[i] == ' '){
-                continue;
+        for(int i = s.length()-1; i >=0; i--) {
+            if(s[i] != ' ') {
+                ++c;
             } else {
-                c++;
+            if (c)
+                return c;    
             }
             
         }
-        if (c == 0) {
-            return last;
-        } else {
-            return c;
-        }
+        return c ;
         
     }
 };
+
+// " a   "
