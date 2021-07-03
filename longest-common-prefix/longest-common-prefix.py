0ddb699dc79df8529d8db94ae6c67018ab5d2289
abahnasy <asbahnasy@gmail.com>
2021-07-03 05:36:42 +0200

    Time: 24 ms (98.58%), Space: 14.6 MB (23.58%) - LeetHub

--- longest-common-prefix/longest-common-prefix.py
+++ longest-common-prefix/longest-common-prefix.py
@@ -1,12 +1,23 @@
 class Solution:
+    
+    def rec_lcp(self, strs: List[str], l: int, r: int) -> str:
+        # base case
+        if l == r:
+            return strs[l]
+        
+        mid = l+(r-l)//2
+        p1 = self.rec_lcp(strs, l, mid)
+        p2 = self.rec_lcp(strs, mid+1, r)
+        
+        min_ = int(min(len(p1), len(p2)))
+        for i in range(min_):
+            if p1[i] != p2[i]:
+                return p1[0:i]
+        
+        return p1[0:min_]
+        
+    
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        str_len = [len(s) for s in strs]
-        min_l = min(str_len)
-        prefix = ""
-        for i in range(min_l):
-            c = strs[0][i]
-            for j in range(1, len(strs)):
-                if strs[j][i] != c:
-                    return prefix
-            prefix = prefix + c
-        return prefix
+        if len(strs) == 0:
+            return ""
+        return self.rec_lcp(strs, 0, len(strs)-1)
