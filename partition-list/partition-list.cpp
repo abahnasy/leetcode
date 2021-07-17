15f731064f1d2765b1400e233549e32784716283
abahnasy <asbahnasy@gmail.com>
2021-07-17 17:23:33 +0200

    Time: 8 ms (48.72%), Space: 10.4 MB (24.99%) - LeetHub

--- partition-list/partition-list.cpp
+++ partition-list/partition-list.cpp
@@ -11,67 +11,107 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
+        
         if(!head || !head->next)
             return head;
-        
-        vector<ListNode*> before;
-        vector<ListNode*> after;
-        ListNode* curr = head;
 
+        
+        ListNode dummy_before = ListNode(0, nullptr);
+        ListNode dummy_after = ListNode(0, nullptr);
+        ListNode* before = &dummy_before;
+        ListNode* after = &dummy_after;
+        
+        ListNode* curr = head;
         while(curr) {
+            
             if(curr->val < x) {
-                before.push_back(curr);
-                cout << "push before\n";
-            } else  {
-                after.push_back(curr);
-                cout << "push after\n";
+                
+                before->next = curr;
+                before = before->next;
+                
+            } else {
+                after->next = curr;
+                after = after->next;
             }
-             curr = curr->next;
-        }
-        cout << " second stage \n";
-        if(before.size() != 0) {
-            cout << "filling befroe \n";
-            head = before[0];
-            curr = head;
-            for(auto e: before) {
-                if (e == head)
-                    continue; // skip the first assignment which happened before
-                curr->next = e;
-                curr = e;
-            }
-        }
-        else {
-            cout << "if no before  \n";
-            head = after[0];
-            curr = head;
-            for(auto e: after) {
-                if (e == head)
-                    continue; // skip the first assignment which happened before
-                curr->next = e;
-                curr = e;
-            }
-        }
-        
-        for(auto e: after) {
-            cout << "after value: " << e->val << "\n";
-            curr->next = e;
-            curr = e;
-        }
-        
-        // derefrence the last node
-        curr->next = nullptr;
-        
-        // debug print
-        cout << "DEBUG PRINT \n";
-        curr = head;
-        while(curr) {
-            cout << curr->val <<", ";
+            
             curr = curr->next;
         }
+        
+        after->next = nullptr;
+        // join
+        before->next = dummy_after.next;
+        return dummy_before.next;
+        
+        
+    }
+};
+
+
+
+// class Solution {
+// public:
+//     ListNode* partition(ListNode* head, int x) {
+//         if(!head || !head->next)
+//             return head;
+        
+//         vector<ListNode*> before;
+//         vector<ListNode*> after;
+//         ListNode* curr = head;
+
+//         while(curr) {
+//             if(curr->val < x) {
+//                 before.push_back(curr);
+//                 // cout << "push before\n";
+//             } else  {
+//                 after.push_back(curr);
+//                 // cout << "push after\n";
+//             }
+//              curr = curr->next;
+//         }
+//         // cout << " second stage \n";
+//         if(before.size() != 0) {
+//             // cout << "filling befroe \n";
+//             head = before[0];
+//             curr = head;
+//             for(auto e: before) {
+//                 if (e == head)
+//                     continue; // skip the first assignment which happened before
+//                 curr->next = e;
+//                 curr = e;
+//             }
+//         }
+//         else {
+//             // cout << "if no before  \n";
+//             head = after[0];
+//             curr = head;
+//             for(auto e: after) {
+//                 if (e == head)
+//                     continue; // skip the first assignment which happened before
+//                 curr->next = e;
+//                 curr = e;
+//             }
+//         }
+        
+//         for(auto e: after) {
+//             // cout << "after value: " << e->val << "\n";
+//             curr->next = e;
+//             curr = e;
+//         }
+        
+//         // derefrence the last node
+//         curr->next = nullptr;
+        
+//         // debug print
+//         // cout << "DEBUG PRINT \n";
+//         curr = head;
+//         while(curr) {
+//             cout << curr->val <<", ";
+//             curr = curr->next;
+//         }
             
         
     
 
-        return head;
-    }
-};
+//         return head;
+//     }
+// };
