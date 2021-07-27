1560bf92449c22f45e31e1f20fde69a99d6706b7
abahnasy <bahnasy.dev@gmail.com>
2021-07-27 23:14:56 +0200

    Time: 0 ms (100.00%), Space: 5.9 MB (22.66%) - LeetHub

--- sqrtx/sqrtx.cpp
+++ sqrtx/sqrtx.cpp
@@ -1,7 +1,24 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        int l = 0;
+        int r = x;
+        while(l < r) {
+            // cout << " l and r " << l << ", " << r << "\n";
+            long mid = (r+ l)/2;
+            if (mid*mid == x) {
+                return mid;
+                
+            } else if (mid*mid > x) {
+                r = mid -1;
+            } else {
+                if((mid+1)*(mid+1)>x)
+                    return mid;
+                l = mid+ 1;
+            }
+        }
+        
+        return l;
         
     }
 };
