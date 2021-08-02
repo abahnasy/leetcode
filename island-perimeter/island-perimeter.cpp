40baa7c829a5ff8bb75c472cdbc2b2fce240d311
abahnasy <bahnasy.dev@gmail.com>
2021-08-02 02:57:33 +0200

    Time: 132 ms (23.32%), Space: 100.9 MB (7.07%) - LeetHub

--- island-perimeter/island-perimeter.cpp
+++ island-perimeter/island-perimeter.cpp
@@ -1,29 +1,47 @@
 class Solution {
 public:
-    vector<vector<int>> dir = {{-1,0}, {1, 0}, {0,-1}, {0,1}};
-    int islandPerimeter(vector<vector<int>>& grid) {
-        int M = grid.size();
-        int N = grid[0].size();
+    
+    int M,N;
+    vector<vector<bool>> visited;
+    vector<vector<int>> dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
+    int dfs(vector<vector<int>>& grid, int i ,int j) {
+        if(i < 0 || i >= M || 
+           j < 0 || j >= N || 
+           grid[i][j] == 0
+          ) {
+            return 1;
+        }
+        
+        if(visited[i][j])
+            return 0;
+                    
+        visited[i][j] = true;
         int c = 0;
-        for(int i = 0; i < M; ++i) {
-            for(int j = 0; j < N; ++j) {
-                if(!grid[i][j])
-                    continue;
-                // 4 corners checks
-                for(int k = 0; k < 4; ++k) {
-                    int new_i = i + dir[k][0];
-                    int new_j = j + dir[k][1];
-                    if(new_i < 0 || new_i >= M || 
-                       new_j < 0 || new_j >= N 
-                       || grid[new_i][new_j] == 0
-                      ) {
-                        ++c;
-                    }
-                }
-                
-            }
+        for( int k = 0; k < 4; ++k ) {
+            int new_i = i + dir[k][0];
+            int new_j = j + dir[k][1];
+            
+            c+= dfs(grid, new_i, new_j);
         }
         
         return c;
+    }
+    
+    int islandPerimeter(vector<vector<int>>& grid) {
+        M = grid.size();
+        N = grid[0].size();
+        visited.resize(M, vector<bool>(N, false));
+        
+        for(int i = 0; i < M; ++i) {
+            for(int j = 0; j < N; ++j) {
+                if(grid[i][j] == 1) {
+                    // assuming only one island
+                    return dfs(grid, i ,j);
+                }
+            }
+        }
+        
+        return 0;
     }
 };
