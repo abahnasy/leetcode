3fd30fb881f49f4b9dddab74365c86e1204eca2d
abahnasy <bahnasy.dev@gmail.com>
2021-08-03 12:16:30 +0200

    Time: 4 ms (61.66%), Space: 16.1 MB (9.22%) - LeetHub

--- subsets/subsets.cpp
+++ subsets/subsets.cpp
@@ -2,30 +2,22 @@ class Solution {
 public:
     vector<vector<int>> res;
     int N;
-    int K;
-    
-    void backtrack(int idx, vector<int>& curr, vector<int>& nums) {
-        if(curr.size() == K) {
-            res.push_back(vector<int>(curr.begin(), curr.end()));
-            return;
+    void helper(vector<int>& nums, vector<int> temp, int idx) {
+        if(idx == N) {
+            res.push_back(temp);
+        } else {
+            helper(nums, temp, idx+1);
+            temp.push_back(nums[idx]);
+            helper(nums, temp, idx+1);
+            temp.erase(temp.end()-1);
         }
-        for(int i = idx; i < N; ++i) {
-            curr.push_back(nums[i]);
-            backtrack(i+1, curr, nums);
-            curr.erase(curr.end()- 1);
-        }
-            
     }
-    
     vector<vector<int>> subsets(vector<int>& nums) {
-        
         N = nums.size();
-        vector<int> curr = {};
-        for(K = 0; K < N+1; ++K) {
-            backtrack(0, curr, nums);
-        }
+        
+        vector<int> temp;
+        helper(nums, temp, 0);
         
         return res;
-        
     }
 };
