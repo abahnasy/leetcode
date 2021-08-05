d114f36d87d6769f9c4b555fc75fce147901f37b
abahnasy <bahnasy.dev@gmail.com>
2021-08-05 13:21:02 +0200

    Time: 24 ms (26.62%), Space: 16.6 MB (12.98%) - LeetHub

--- stone-game/stone-game.cpp
+++ stone-game/stone-game.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+    
+    int help(vector<int>& piles, int i, int j) {
+        if(i > j)
+            return 0;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        else {
+            dp[i][j] = max(piles[i] - help(piles, i+1, j), 
+                           piles[j] - help(piles, i, j-1)
+                          );
+            return dp[i][j];
+        }
+        
+    }
+    
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int N = piles.size();
+        dp.resize(N, vector<int>(N, -1));
+        
+        return help(piles, 0, N-1) > 0;
+   
+   
+   
+        
+         
     }
 };
