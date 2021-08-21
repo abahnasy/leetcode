c2ded88cfeb7e00349b82d6f2ec2b24b7bc3147b
abahnasy <bahnasy.dev@gmail.com>
2021-08-21 13:03:53 +0200

    Time: 213 ms (8.69%), Space: 14.3 MB (61.54%) - LeetHub

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
-    unordered_map<int, int> dp;
-    int rec(vector<int>& coins, int amount) {
-        // for(int i = 0; i < level; ++i)
-        //     cout << "  ";
-        // cout << "exploring " << amount << "\n";
-        if(amount == 0)
-            return 0;
-        if(dp.find(amount) != dp.end())
-            return dp[amount];
-        
-        int min_c = INT_MAX;
-        for(auto coin: coins) {
-            if(coin > amount)
-                continue;
-            int val = rec(coins, amount - coin);
-            min_c = min(min_c, val);
-        }
-        
-        min_c = (min_c == INT_MAX)?min_c: min_c+1;
-        dp[amount] = min_c;
-        return min_c;
-    }
     int coinChange(vector<int>& coins, int amount) {
-        if(amount == 0)
-            return 0;
-        int res =  rec(coins, amount);
-        if(res == INT_MAX)
-            return -1;
-        else
-            return res;
-        
+        int Max = amount + 1;
+        vector<int> dp(amount + 1, Max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.size(); j++) {
+                if (coins[j] <= i) {
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     int coinChange(vector<int>& coins, int amount) {
+//         // choose upperbound value, either amount+1 or INT_MAX
+//         vector<int>dp(amount+1, INT_MAX);
+//         dp[0] = 0;
+//         for(int i = 0; i <= amount; ++i) {
+//             for(auto coin: coins) {
+//                 if(i-coin <0)
+//                     continue;
+//                 cout << "filling " << i << "\n";
+//                 dp[i] = min(dp[i-coin]+1, dp[i]);
+//             }
+//             cout << "final: dp[" << i << "] = " << dp[i] << "\n"; 
+//         }
+        
+//         return (dp[amount] == amount+1)?-1:dp[amount];
+//     }
+// };
