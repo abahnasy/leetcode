0b5f42cae208543467e809664695365e0cf0863c
abahnasy <bahnasy.dev@gmail.com>
2021-08-21 12:33:12 +0200

    Time: 758 ms (5.01%), Space: 63.1 MB (5.17%) - LeetHub

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -1,26 +1,35 @@
-typedef vector<int> vi;
-typedef vector<vector<int>> vii;
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        sort(coins.begin(), coins.end());
-        vi dp(amount+1, amount+1);
-        dp[0] = 0;
-        for (int i = 0; i <=  amount; ++i ) {
-            for (int j = 0; j < coins.size(); ++j) {
-                if(i >= coins[j]) {
-                    dp[i] = min(dp[i] , dp[i-coins[j]] +1);
-                } else {
-                    break;
-                }
-            }
+    unordered_map<int, int> dp;
+    int rec(vector<int>& coins, int amount) {
+        // for(int i = 0; i < level; ++i)
+        //     cout << "  ";
+        // cout << "exploring " << amount << "\n";
+        if(amount == 0)
+            return 0;
+        if(dp.find(amount) != dp.end())
+            return dp[amount];
+        
+        int min_c = INT_MAX;
+        for(auto coin: coins) {
+            if(coin > amount)
+                continue;
+            int val = rec(coins, amount - coin);
+            min_c = min(min_c, val);
         }
         
-        if(dp[amount] <= amount) {
-            return dp[amount];
-        } else {
+        min_c = (min_c == INT_MAX)?min_c: min_c+1;
+        dp[amount] = min_c;
+        return min_c;
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        if(amount == 0)
+            return 0;
+        int res =  rec(coins, amount);
+        if(res == INT_MAX)
             return -1;
-        }
+        else
+            return res;
         
     }
 };
