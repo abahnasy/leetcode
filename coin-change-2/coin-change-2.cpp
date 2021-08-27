517aafce1ba3d5654e56f36ad75523c823ce28d9
abahnasy <bahnasy.dev@gmail.com>
2021-08-27 12:29:15 +0200

    Time: 42 ms (21.06%), Space: 18.6 MB (28.39%) - LeetHub

--- coin-change-2/coin-change-2.cpp
+++ coin-change-2/coin-change-2.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
+        vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1, 0));
         
-        vector<vector<int>>dp (coins.size()+1, vector<int>(amount+1, 0));
-        
-        for(int i = 0; i < coins.size()+1; ++i) {
+        // loop over coins with amount = 0
+        for(int i =0; i < coins.size()+1 ;++i) {
             dp[i][0] = 1;
         }
+        // loop over amounts with no coins
         
         for(int j = 1; j < amount+1; ++j) {
-            dp[0][j] = 0; 
+            dp[0][j] = 0;
         }
         
-        
-        for(int i =1; i <= coins.size(); ++i) {
-            for(int j = 1; j <= amount; ++j) {
-                // cout << "accessing: " << i <<", " << j << "\n";
-                // cout << "coin: "<< coins[i-1] << " amount: " << j << "\n";
+        for(int i = 1; i < coins.size()+1 ;++i) {
+            for(int j = 1; j < amount+1; ++j) {
+                // cout << i << ", " << j << endl;
                 if(coins[i-1] > j) {
-                    // cout << "coin " << coins[i-1] << "is greater than the amount " << amount << "\n";
-                    
-                    dp[i][j] = dp[i-1][j];
+                    dp[i][j] = dp[i-1][j];    
                 } else {
-                    int col = j - coins[i-1];
-                    // cout << "Value after using the coin " << coins[i-1] << "is " << dp[i][col] << "\n";
-                    // cout << "accessing: " << i <<", " << j << "\n";
-                    dp[i][j] = dp[i-1][j] + dp[i][col];    
+                    dp[i][j] = dp[i-1][j] + dp[i][j- coins[i-1]];
                 }
                 
             }
         }
         
-        return dp[coins.size()][amount];
         
+        return dp[coins.size()][amount];
     }
 };
