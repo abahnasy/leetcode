f43bfa6df3220ae0ca91239f8dcc0b0692cee93f
abahnasy <bahnasy.dev@gmail.com>
2021-08-27 23:05:45 +0200

    Time: 34 ms (5.05%), Space: 15.6 MB (5.10%) - LeetHub

--- min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
+    unordered_map <int, int >dp;
+    int rec (vector<int>& cost, int n) {
+        if(n >= cost.size())
+            return 0;
+        if(dp.find(n) != dp.end())
+            return dp[n];
+        dp[n] =cost[n] + min(rec(cost, n+1), rec(cost, n+2)); 
+        return dp[n];
+    }
     int minCostClimbingStairs(vector<int>& cost) {
-        if(cost.size() == 2) {
-            return (cost[0] > cost[1])? cost[1]: cost[0];
-        } else {
-            for (int i =2; i < cost.size(); ++i) {
-                cost[i] += min(cost[i-1], cost[i-2]);
-            }
-            
-            return min(cost[cost.size()-1], cost[cost.size()-2]);
-        }
+        if(cost.size() == 0)
+            return 0;
+        return min (rec(cost, 0), rec(cost, 1));
     }
 };
-
-
-// class Solution {
-// public:
-//     int minCostClimbingStairs(vector<int>& cost) {
-//         // reverse solution starting from the end
-//         // this might be useful in case cost array is not editable
-//         int step1 = 0;
-//         int step2 = 0;
-//         for(int i= cost.size()-1; i>=0;--i) {
-//             int curr_step = cost[i] + min(step1, step2);
-//             step1=step2;
-//             step2=curr_step;
-//         }
-//         return min(step1, step2);
-            
-//     }
-// };
