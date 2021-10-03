4b67a332794244de312911ef770621873c274c76
abahnasy <12660989+abahnasy@users.noreply.github.com>
2021-10-03 14:01:29 +0200

    Time: 12 ms (89.58%), Space: 16.4 MB (71.61%) - LeetHub

--- jump-game-ii/jump-game-ii.cpp
+++ jump-game-ii/jump-game-ii.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n = nums.size(), step = 0, start = 0, end = 0;
-        while (end < n - 1) {
-            step++; 
-			int maxend = end + 1;
-			for (int i = start; i <= end; i++) {
-                if (i + nums[i] >= n - 1) return step;
-				maxend = max(maxend, i + nums[i]);
-			}
-            start = end + 1;
-            end = maxend;
+        int l = 0; int r = 0;
+        int steps = 0;
+        while(r < nums.size()-1) {
+            // cout << "range: " << l << ", " << r << "\n";
+            int max_r = 0;
+            for(int i = l; i <= r; ++i) {
+                max_r = max(max_r, i + nums[i]);
+                // cout << "done\n";
+            }
+            ++steps;
+            l = r+1;
+            r = max_r;
         }
-		return step;
+        
+        return steps;
+        
     }
 };
