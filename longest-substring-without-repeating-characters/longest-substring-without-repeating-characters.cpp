4c116b4ecc2cbbc710724ae07f7b2368a651d7c1
abahnasy <12660989+abahnasy@users.noreply.github.com>
2021-10-24 23:08:26 +0200

    Time: 106 ms (12.63%), Space: 8.3 MB (57.50%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        if(s.size() == 0) return 0; // edge case
-        unordered_map<char, int>m;
+    int lengthOfLongestSubstring(string s) {        
+        if(s.size() <= 1) return s.size();
+        unordered_map<char, int> m;
         
-        int _max = 0;
-        int l = 0;
-        for(int i = 0; i < s.size(); ++i) {
-            // if c is not seen
-            if(m.find(s[i]) == m.end())
-                _max = max(_max, i-l+1);
-            else {
-                if(m[s[i]] < l) {
-                    _max = max(_max, i-l+1);
-                } else {
-                    l = m[s[i]] + 1;
-                }
+        int max_ = 0;
+        
+        for(int i = 0, j = 0; i < s.size(); ++i) {
+            if(m.count(s[i]) != 0) {     
+                j = max(m[s[i]]+1, j);
+                
             }
-            
-            m[s[i]] = i;
-                // before l
-                // after l
+            m[s[i]]= i;
+            max_ = max(max_, i-j+1);
+            cout << max_ << "\n";
             
         }
         
-        return _max;
+        return max_;
+        
     }
 };
+// map<char, int> m;
+// max = 2
 
-//idx                 |
-//      a b c a b c b b
-//                    l
+//        |
+// "a b b a"
+//     |
